5caeb9edef4527f60a9c61bd4063f5c1a7081f65
nanadarby <natashalodarby@gmail.com>
2025-08-14 13:37:41 +0100

    Created functions to show time and 5 day forecast with corresponding icons

--- src/index.js
+++ src/index.js
@@ -50,6 +50,14 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = `bobc2549412899ebdt7cc792efa3304c`;
   let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -61,17 +69,25 @@ function displayForecast(response) {
   let days = ["Tues", "Wed", "Thurs", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">🌥️</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <div><img src= "${
+              day.condition.icon_url
+            }" class="weather-forecast-icon" /></div>
           <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-temperature"><strong>15°</strong></div>
-            <div class="weather-forecast-temperature">9°</div>
+            <div class="weather-forecast-temperature"><strong>${Math.round(
+              day.temperature.maximum
+            )}°</strong></div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+            )}°</div>
           </div>
           </div>`;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
 
